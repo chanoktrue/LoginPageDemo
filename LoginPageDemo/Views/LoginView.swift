ad103156d07ed3fe7bea4d73d5a0966c09671ceb
chanoktrue <chanoktrue@me.com>
2024-01-21 07:22:52 +0700

    first commit

---
+++ LoginPageDemo/Views/LoginView.swift
@@ -0,0 +1,18 @@
+//
+//  LoginView.swift
+//  LoginPageDemo
+//
+//  Created by Thongchai Subsaidee on 21/1/24.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoginView()
+}
