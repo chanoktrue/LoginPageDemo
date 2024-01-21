651128fc4606bcfc807404aa1e42297c8544172f
chanoktrue <chanoktrue@me.com>
2024-01-21 07:20:19 +0700

    Initial Commit

---
+++ LoginPageDemo/LoginPageDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginPageDemoApp.swift
+//  LoginPageDemo
+//
+//  Created by Thongchai Subsaidee on 21/1/24.
+//
+
+import SwiftUI
+
+@main
+struct LoginPageDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
